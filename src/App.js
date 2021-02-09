4ea943ba56851e723f6ca03ea0932b9808cb0db8
dannyo1 <dannyo1@live.unc.edu>
2021-02-08 21:57:22 -0500

    done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ const App = () => {
     <div className="home">
       {/* <Header /> */}
 
+      
+
       <div id="content">
+      <img src ="../images/olliebaby.png" alt="ollie" width = "250" height = "200"></img>
+      <img src ="../images/wxyc.png" alt="wxyc" width = "250" height = "200"></img>
+      <img src ="../images/turntable.png" alt="spins" width = "400" height = "200"></img>
+      <img src ="../images/totoro.png" alt="neighbor" width = "250" height = "200"></img>
+      <img src ="../images/spider.png" alt="spidey" width = "250" height = "200"></img>
         <ReactMarkdown source={lesson_one} />
       </div>
 
